143c1f58de84faa5b87b23f722b33c18e20e1c4b
Rodrigo Escobar <rodrigo.escobar.z@usach.cl>
2025-07-18 20:14:11 -0400

    Fix años
    Se modifican los años disponibles en vista de PCA y Cluster

--- frontend/src/components/PcaClusterView.jsx
+++ frontend/src/components/PcaClusterView.jsx
@@ -171,7 +171,7 @@ const PcaClusterView = ({ onNavigate }) => {
     }
   }; // Años disponibles
   const availableYears = [
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024,
+    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023,
   ];
 
   // Colores para clusters
